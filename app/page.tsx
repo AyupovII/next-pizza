5da1d70a65e8aa5a328c79fb2564066ae49de937
Aupov_Ilgiz <solomonra95@gmail.com>
2024-08-07 18:03:05 +0500

    develop

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,27 @@
-import { Container, Title } from "@/components/shared";
+import { Categories, Container, Filters, Title } from "@/components/shared";
+import { SortPopup } from "@/components/shared/sort-popup";
+import { TopBar } from "@/components/shared/top-bar";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-    <Container className="mt-10">
-      <Title text="Все пиццы" size="lg" className="font-extralight"/>
-    </Container>
+      <Container className="mt-10">
+        <Title text="Все пиццы" size="lg" className="font-extralight" />
+      </Container>
+      <TopBar />
+      <Container className="mt-10 pb-14">
+        <div className="flex gap-[60px]">
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+        </div>
+        <div className="flex flex-col gap-16">
+          Список товаров
+        </div>
+
+      </Container>
     </>
 
   );
